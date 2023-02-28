1d58a2cd557ae7948482f18fa3f913c65a399cf6
Rey V <radiativedreams@gmail.com>
2023-02-28 20:38:24 +0000

    Completed Hardcoded Login

--- src/main/java/Lab.java
+++ src/main/java/Lab.java
@@ -15,6 +15,9 @@ public class Lab {
      * @return true if there is a successful login, and false otherwise.
      */
     public boolean login(String username, String password){
-        return false;
+        //return ( (username == "admin" && password == "qwerty") || (username == "user" && password == "password") );
+        if(username == "admin" && password == "qwerty") return true;
+        else if(username == "user" && password =="password") return true;
+        else return false;
     }
 }
